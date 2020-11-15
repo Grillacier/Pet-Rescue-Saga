6c3738d7499667ab8511c6b95b9504071b4160bc
SILVESTRE alaia <alaia98@gmail.com>
2020-11-15 18:19:03 +0100

    classe Bloc qui hérite de Case

---
+++ Bloc.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Bloc extends Case{
+    //attributs
+    private String couleur;
+
+    //constructeur
+    public Bloc() {
+        colorier();
+    }
+
+    public static Random rand = new Random();
+
+    // permet de tirer des entiers au hasard entre a inclus et b exclus
+    public static int randRange(int a, int b) {
+        return rand.nextInt(b-a)+a;
+    }
+
+    public void colorier(){
+        int c = randRange(1, 6);
+        switch (c) {
+            case 1: this.couleur = "Rouge"; break;
+            case 2: this.couleur = "Bleu"; break;
+            case 3: this.couleur = "Jaune"; break;
+            case 4: this.couleur = "Vert"; break;
+            case 5: this.couleur = "Violet"; break;
+        }
+    }
+
+    //accesseurs
+    public String getCouleur() {
+        return couleur;
+    }
+}
