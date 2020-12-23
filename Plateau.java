503f3fe1ab433f9c1a10f2ac61c3a34e8e0a2561
SILVESTRE alaia <alaia98@gmail.com>
2020-12-23 17:25:24 +0100

    Update Plateau.java

--- Plateau.java
+++ Plateau.java
@@ -4,26 +4,36 @@ public class Plateau {
     //attributs
     private final int hauteur;
     private final int largeur = 9;
-    private Case[][] case;
+    private Case[][] cases;
 
     //constructeur
     public Plateau() {
         Random rd = new Random();
         this.hauteur = rd.nextInt(11-10+1)+10;
-        this.case = new Case [hauteur] [largeur];
+        this.cases = new Case[hauteur][largeur];
         ajouteBlocs();
+        ajouteAnimaux();
     }
 
     //afficher le plateau
     public void affiche() {
-        //ligne contenant les animaux, fonction à définir plus tard
-        for (int a = 0; a < this.largeur; a++)
-            System.out.print("Animal" + " ");
+        String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int coord = 1;
+        System.out.print("     ");
+        for (int i = 0; i < largeur; i++)
+            System.out.print(coord++ + "   ");
+        System.out.println();
+        System.out.print("   ");
+        for (int i = 0; i < largeur; i++)
+            System.out.print("----");
         System.out.println();
 
-        for (int i = 1; i < this.hauteur; i++) {
+        for (int i = 0; i < this.hauteur; i++) {
+            System.out.print(alpha.charAt(i) + " ");
+            System.out.print("| ");
             for (int j = 0; j < this.largeur; j++) {
-                System.out.print(case [i][j].getCouleur() + " ");
+                if (cases[i][j] != null)
+                    System.out.print(cases[i][j].getCouleur() + " ");
             }
             System.out.println();
         }
@@ -31,14 +41,15 @@ public class Plateau {
 
     //placement des animaux
     private void ajouteAnimaux() {
-
+        for (int a = 0; a < this.largeur; a++)
+            this.cases[0][a] = new Animal();
     }
 
     //placement des blocs dans le plateau
     private void ajouteBlocs() {
-        for (int i = 1; i < this.blocs.length; i++) {
-            for (int j = 0; j < this.blocs[i].length; j++) {
-                this.blocs[i][j] = new Bloc();
+        for (int i = 1; i < this.cases.length; i++) {
+            for (int j = 0; j < this.cases[i].length; j++) {
+                this.cases[i][j] = new Bloc();
             }
         }
     }
