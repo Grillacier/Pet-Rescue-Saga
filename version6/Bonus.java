554536b01df893e8c3383b0dee04501898ee0208
SILVESTRE alaia <alaia98@gmail.com>
2021-01-03 06:02:03 +0100

    Téléverser un nouveau fichier

---
+++ version6/Bonus.java
@@ -0,0 +1,14 @@
+public class Bonus {
+    //attribut
+    private String type;
+
+    //constructeur
+    public Bonus(String type) {
+        this.type = type;
+    }
+
+    //accesseur
+    public String getType() {
+        return type;
+    }
+}
