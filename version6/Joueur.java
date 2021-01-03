21c9dce7e36c38e951347396d138336e7a616734
SILVESTRE alaia <alaia98@gmail.com>
2021-01-03 06:01:23 +0100

    Téléverser un nouveau fichier

---
+++ version6/Joueur.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Joueur {
+    //attribut
+    private Scanner scanReponse;
+
+    //constructeur
+    public Joueur() {
+        this.scanReponse = new Scanner(System.in);
+    }
+
+    //fonction pour récupérer ce qui est entré au clavier
+    private String demanderStr(String q) {
+        System.out.println(q);
+        return scanReponse.next();
+    }
+
+    //on demande au joueur quel bloc il veut détruire
+    public int[] demanderCoordonnes() {
+        int[] tab = new int[2];
+        String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String coord = demanderStr("Entrez des coordonnées (ex : B6)");
+        for (int i = 0; i < alpha.length(); i++) {
+            if (coord.charAt(0) == alpha.charAt(i))
+                tab[0] = i;
+        }
+        tab[1] = Character.getNumericValue(coord.charAt(1))-1;
+        return tab;
+    }
+
+    public void finir() {
+        this.scanReponse.close();
+    }
+}
